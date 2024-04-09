fa37bfd1a237877975a7fcbabfe6fc5fbcb8326d
stinson-daniel <stinson.daniel623@gmail.com>
2024-04-09 15:23:21 -0400

    add comments to start logic

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-console.log("This is a test.")
+// get computer's choice
+    // 1-rock 2-paper 3-scissors
+
+// get players choice
+    // 1-rock 2-paper 3-scissors
+
+// check computer choice against player
+    // computer win
+    // computer lose
+    // tie
